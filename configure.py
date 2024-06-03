834db521feefcba8f5db7d499151a11d17d6f228
Frankie32244 <90593805+Frankie32244@users.noreply.github.com>
2024-06-03 14:15:20 +0800

    Update configure.py

--- configure.py
+++ configure.py
@@ -16,17 +16,17 @@ def get_default_config(data_name):
                 arch2=[512, 1024, 1024, 1024, 128],  # the last number in arch1 and arch2 should be the same
                 activations1='relu',                 # 表示在 arch1 子网络的每一层都应用 ReLU 激活函数。
                 activations2='relu',                 # 表示在 arch2 子网络的每一层也都应用 ReLU 激活函数。
-                batchnorm=True,                      # 批归一化处理
+                batchnorm=True,                      # 批归一化处理,显著提升模型的训练性能和泛化能力
             ),
             training=dict(
-                seed=4,
-                missing_rate=0.5,
-                start_dual_prediction=100,
-                batch_size=256,
-                epoch=500,
-                lr=1.0e-4,
-                # Balanced factors for L_cd, L_pre, and L_rec
-                alpha=9,
+                seed=4,                              # 随机种子=4，确保实验的可重复性
+                missing_rate=0.5,                    # 缺失率
+                start_dual_prediction=100,           # 表示在训练到第100个epoch时开始进行双重预测。
+                batch_size=256,                      # batch_size=256 表示每次迭代使用256个样本进行训练
+                epoch=500,                           # epoch=500 表示要训练500个轮次。
+                lr=1.0e-4,                           # lr=1.0e-4 表示学习率为0.0001。
+                # Balanced factors for L_cd, L_pre, and L_rec  文中的其它温度平衡参数
+                alpha=9,                        
                 lambda1=0.1,
                 lambda2=0.1,
             ),
